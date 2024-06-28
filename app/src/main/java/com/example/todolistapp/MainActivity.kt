489891a22e3db070543989b550399bd5ea707e28
zeff96 <zeffo1975@gmail.com>
2024-06-28 18:28:12 +0300

    Add todos to the ui

--- app/src/main/java/com/example/todolistapp/MainActivity.kt
+++ app/src/main/java/com/example/todolistapp/MainActivity.kt
@@ -1,47 +1,31 @@
 package com.example.todolistapp
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.todolistapp.ui.theme.TodoListAppTheme
 
 class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             TodoListAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+               Surface(modifier = Modifier.fillMaxSize()
+                   .padding(top = 40.dp)
+               ) {
+                   TodosLists(todos = SampleData.sampleTodos)
+               }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TodoListAppTheme {
-        Greeting("Android")
     }
 }
