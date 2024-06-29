138070194d0b487768ffd70055a7bfe1df67da79
zeff96 <zeffo1975@gmail.com>
2024-06-29 16:27:00 +0300

    Add user input field

--- app/src/main/java/com/example/todolistapp/TodoListPage.kt
+++ app/src/main/java/com/example/todolistapp/TodoListPage.kt
@@ -3,6 +3,7 @@ package com.example.todolistapp
 import android.os.Build
 import androidx.annotation.RequiresApi
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
@@ -11,11 +12,17 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -36,9 +43,7 @@ fun TodoListsCard(todo: Todo){
             .padding(8.dp)
             .clip(RoundedCornerShape(16.dp))
             .background(MaterialTheme.colorScheme.primary)
-            .padding(
-                16.dp,
-            ),
+            .padding(16.dp),
         verticalAlignment = Alignment.CenterVertically
     ) {
         Column(modifier = Modifier.weight(1f)) {
@@ -82,10 +87,28 @@ fun TodosLists(todos: List<Todo>){
 @RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun TodoListPage(){
+    var inputText by remember {
+        mutableStateOf("")
+    }
     Column(modifier = Modifier
         .fillMaxHeight()
         .padding(8.dp)
     ) {
+        Row(
+            modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp),
+            horizontalArrangement = Arrangement.SpaceEvenly
+        ) {
+            OutlinedTextField(
+                value = inputText,
+                onValueChange = {
+                inputText = it
+            })
+            Button(onClick = { /*TODO*/ }) {
+                Text(text = "Add")
+            }
+        }
         TodosLists(todos = SampleData.sampleTodos)
     }
 }
