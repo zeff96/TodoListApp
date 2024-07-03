1a3a6868501c8c4e4d4d9219890cb21b522ba59e
zeff96 <zeffo1975@gmail.com>
2024-07-03 15:54:27 +0300

    Add Scaffold to mak ui

--- app/src/main/java/com/example/todolistapp/MainActivity.kt
+++ app/src/main/java/com/example/todolistapp/MainActivity.kt
@@ -6,14 +6,19 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.annotation.RequiresApi
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.ViewModelProvider
 import com.example.todolistapp.ui.theme.TodoListAppTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,11 +26,22 @@ class MainActivity : ComponentActivity() {
         val dataModel = ViewModelProvider(this)[TodoViewModel::class.java]
         setContent {
             TodoListAppTheme {
-               Surface(
-                   modifier = Modifier.fillMaxSize(),
-                   color = MaterialTheme.colorScheme.background
-               ) {
-                   TodoListPage(dataModel)
+               Scaffold(
+                   topBar = {
+                       TopAppBar(
+                           colors = topAppBarColors(
+                               titleContentColor = MaterialTheme.colorScheme.primary
+                           ),
+                           title = {
+                               Text(text = "Tasks")
+                           }
+                       )
+                   }
+               ) {innerPadding->
+                   TodoListPage(
+                       dataModel,
+                       modifier = Modifier.padding(innerPadding)
+                   )
                }
             }
         }
